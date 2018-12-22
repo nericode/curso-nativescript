1d61d0d024f479e8e06f2d0ae01e4b780d0adfe8
Alan <alanneri10@gmail.com>
2018-12-22 10:26:43 -0600

    [fix][import module]

--- parte-4/src/app/app.module.ts
+++ parte-4/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
@@ -23,7 +22,6 @@ import { ItemDetailComponent } from "./item/item-detail.component";
     declarations: [
         AppComponent,
         ItemsComponent,
-        ItemDetailComponent
     ],
     providers: [],
     schemas: [
